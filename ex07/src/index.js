c2a7519a26656dc2caca134982c61e87b7560170
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-16 19:58:50 +0200

    Finding a Remainder

---
+++ ex07/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo){
+    // only change code below this line
+    var remainder;
+    var numOne;
+    var numTwo;
+    remainder = numOne % numTwo;
+    // only change code above this line
+    return remainder;
+}
+
+console.log(5 % 2);
+console.log(15 % 4);
+console.log(102 % 25);
+module.exports = main;
