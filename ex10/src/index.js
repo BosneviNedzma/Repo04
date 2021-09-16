2bb53025fcdce00264624ecdf5432891d5e1844e
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-16 20:28:53 +0200

    Declare String Variables

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // only change code below this line
+    var myFirstName = "Nedžma";
+    var myLastName = "Brkić";
+    var myHomeTown = "Busovača";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
