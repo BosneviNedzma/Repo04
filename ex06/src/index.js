1d11e850d7600c431b9e29764febf1a8a49761ff
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-16 19:23:12 +0200

    Divide One Decimal by Another with JS

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo){
+    // only change code below this line
+    var numOne;
+    var namTwo;
+    var myQuotient = numOne / numTwo;
+    // only change code above this line
+    return myQuotient;
+}
+
+console.log(5.0 / 2.0);
+console.log(16.5 / 5.5);
+console.log(102.0 / 25.5);
+module.exports = main;
